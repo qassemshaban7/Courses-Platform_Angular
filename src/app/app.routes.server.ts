7fe170e9a1a38cd0148814e0c4383e99e21ca85e
qassemshaban7 <qassemshaban7@gmail.com>
2025-04-03 00:03:48 +0200

    edit routes

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -6,6 +6,38 @@ export const serverRoutes: ServerRoute[] = [
     renderMode: RenderMode.Client
   },
   {
+    path: 'enterpin/:email/:expireAt',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'forgetpassword/:email',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'admin/editcourse/:id',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'admin/editinstructor/:id',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'admin/addlesson/:courseid',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'admin/editlesson/:id/:courseId',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'admin/showlesson/:id/:courseId',
+    renderMode: RenderMode.Client
+  },
+  {
+    path: 'admin/lessons/:courseid',
+    renderMode: RenderMode.Client
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
